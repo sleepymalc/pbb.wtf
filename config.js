5aba20e7f83110eb896fefc209f14c9c11f2023b
Pingbang Hu <sleepymalc@gmail.com>
2022-10-15 02:27:33 -0400

    📍[main/]

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   authorAlternative: 'Write-The-Formula',
   introduction: [
     'Hi there 👋! My name is **Pingbang** (**胡平邦**), pronounced as *Hu, Pingˊ-Bongˉ*, from Taiwan 🇹🇼, and I\'m a Bachelor student major in **Computer Science** and **Honor Mathematics** in *University of Michigan (UMich)* 🇺🇸 and **Electrical Computer Engineering** in *Shanghai Jiao Tong University (SJTU)* 🇨🇳. My research interest is about *provable modern machine learning theory* and also *theoretical computer science*, specialized in *graph theory* in particular.',
-    'Despite my research, my interest spreads widely among topics in TCS and mathematics such as approximation, online and randomized algorithm 🧠; random matrix theory, high dimensional statistic, differential and algebraic topology and functional analysis 📚.',
-    'Occasionally, I\'ll do [street photography](https://www.instagram.com/malc_ph/) 📷 and write [blog posts](./posts) 📝!',
+    'Despite my research, my interest spreads widely among topics in TCS and mathematics such as approximation, online and randomized algorithm 🧠; random matrix theory, high dimensional statistic, differential and algebraic topology and functional analysis 📚',
+    'Occasionally, I\'ll do [street photography](https://www.instagram.com/malc_ph/) 📷 and write [blog posts](./posts) 📝',
   ],
   avatar: 'avatar.png',
   professions: [
