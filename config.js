454598ed70249dfa94cc1b0d655bca58d3bc5214
Pingbang Hu <sleepymalc@gmail.com>
2023-07-08 23:38:52 +0800

    📍[main/]

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   authorAlternative: 'Write-The-Formula',
   introduction: [
     'Hi 👋! I\'m **Pingbang Hu 胡平邦** (*Hu, Pingˊ-Bongˉ*) from Taiwan 🇹🇼, an incoming Ph.D.student at *University of Illinois Urbana-Champaign* 🌽 advised by Prof. [Jiaqi Ma](https://www.jiaqima.com/). I obtained my [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) at *University of Michigan* 〽️ and *Shanghai Jiao Tong University* 🇨🇳.',
-    `In my spare time, I\'ll do [*photography*](https://street.pbb.wtf/) 📷 and write [*blog posts*](./posts) 📝; otherwise, I\'ll do *AI theory* and *TCS* on *graphs* with *math*. Specifically, I enjoy thinking about *graph neural networks* 🤖, various *algorithms* with their *computational complexity* 🧠, and *high dimensional statistic* 📊.`,
+    `In my spare time, I do [*photography*](https://street.pbb.wtf/) 📷 and write [*blog posts*](./posts) 📝; otherwise, I\'ll do **AI theory** and **TCS** on *graphs* with *math*. Specifically, I enjoy thinking about *graph neural networks* 🤖, various *algorithms* with their *computational complexity* 🧠, and *high dimensional statistic* 📊.`,
     '***Now***, I\'m: designing *fast graph algorithms*, exploring *machine unlearning theory*, and studying *graphings*.',
   ],
   avatar: 'avatar.png',
