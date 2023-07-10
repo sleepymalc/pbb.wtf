82054818f15a23f0e4a53919cae7e13f123e7eb9
Pingbang Hu <sleepymalc@gmail.com>
2023-07-10 18:05:08 +0800

    📍[main/]

--- config.js
+++ config.js
@@ -67,12 +67,12 @@ module.exports = {
     {
       date: 'Sep. 2021 - Apr. 2023',
       icon: 'university',
-      title: 'B.Sc. in Computer Science \& Mathematics (Minor)',
+      title: 'B.Sc. in Computer Science *minor* in Mathematics',
       location: 'College of Engineering \& College of Literature, Science, and the Arts, University of Michigan',
     }, {
       date: 'Sep. 2019 - Aug. 2023',
       icon: 'university',
-      title: 'B.Eng. in Electrical Computer Engineering \& Computer Science (Minor)',
+      title: 'B.Eng. in Electrical Computer Engineering *minor* in Computer Science',
       location: 'UM-SJTU Joint Institute, Shanghai Jiao Tong University',
     }],
   interests: [
