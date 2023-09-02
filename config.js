6fb184cd92f86e881cd0dda9acfc03611834ea02
Pingbang Hu <sleepymalc@gmail.com>
2023-09-02 04:14:48 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   authorAlternative: 'Write-The-Formula',
   introduction: [
     'A first year Ph.D. student at *University of Illinois Urbana-Champaign* 🌽 holding a [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from *University of Michigan* 〽️ and *Shanghai Jiao Tong University* 🇨🇳. I am fortunate enough to be advised by [Jiaqi Ma](https://www.jiaqima.com/).',
-    `I do *AI* and *TCS* with **math** on **graphs**. In general, I try to *theoretically* understand algorithms on graphs with their provable guarantee on, e.g., *robustness*, *fairness*, etc. I'm currently designing *unlearning approximation algorithms* and characterizing *topologies of graph limits*.`,
-    `I think about many problems, while some of them haunt me more than others, e.g., *What is the analogy of **computational complexity theory** for modern graph learning problems?*, i.e., how much can we say about the *hardness* of a graph learning problem?`,
+    `I do *AI* and *TCS* with **math** on **graphs**. In general, I try to *theoretically* understand algorithms with their provable guarantee on, e.g., *robustness*, *fairness*. I'm currently designing *unlearning approximation algorithms* and characterizing *topologies of graph limits*.`,
+    `I think about many problems, while some of them haunt me more than others, e.g., *What is the analogy of **computational complexity theory** for modern (deep) learning problems?*, i.e., how much can we say about the *hardness* of deep learning problems?`,
     `In my spare time, I enjoy [*photography*](https://street.pbb.wtf/) 📷, writing [*random posts*](./posts) 📝, and staying up late 🌌.`
   ],
   avatar: 'avatar.png',
