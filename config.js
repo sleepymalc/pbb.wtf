2ddbe7f193c75cc585213960f1cb4c3db11bdf34
Pingbang Hu <sleepymalc@gmail.com>
2023-09-02 16:01:01 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ module.exports = {
   authorAlternative: 'Write-The-Formula',
   introduction: [
     'I\'m a first year Ph.D. student at *University of Illinois Urbana-Champaign* 🌽, holding a [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from *University of Michigan* 〽️ and *Shanghai Jiao Tong University* 🇨🇳. I\'m fortunate enough to be advised by [Jiaqi Ma](https://www.jiaqima.com/).',
-    `I do *AI* and *TCS* with **math** on **graphs**. In general, I try to design and understand algorithms by proving its *robustness*, *fairness*, etc. I'm currently focusing on *unlearning problems* and *sparse graph limits*.`,
-    `I think about many problems, and a particular one of them haunts me more than others: *the analogy of **computational complexity theory** for modern learning problems*, i.e., how to establish the *hardness* hierarchy of deep learning problems?`,
+    `I do *AI* and *TCS* with **math** on **graphs**. In general, I try to design and understand algorithms by proving its *robustness*, *fairness*, etc. I think about many problems, and a particular one of them haunts me more than others is the *analogy of **computational complexity theory** for modern learning problems*, i.e., how to establish a *hardness* hierarchy of deep learning problems?`,
     `In my spare time, I enjoy [*photography*](https://street.pbb.wtf/) 📷, writing [*random posts*](./posts) 📝, and staying up late 🌌.`
   ],
   avatar: 'avatar.png',
