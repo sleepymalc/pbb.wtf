b5a07a47c98c6755557d8b8abd76130ec6455012
Pingbang Hu <sleepymalc@gmail.com>
2023-09-02 04:32:21 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   introduction: [
     'A first year Ph.D. student at *University of Illinois Urbana-Champaign* 🌽 holding a [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from *University of Michigan* 〽️ and *Shanghai Jiao Tong University* 🇨🇳. I am fortunate enough to be advised by [Jiaqi Ma](https://www.jiaqima.com/).',
     `I do *AI* and *TCS* with **math** on **graphs**. In general, I try to design and understand algorithms by *theoretically* proving its, e.g., *robustness*, *fairness*, etc. I'm currently focusing on *unlearning problems* and *sparse graph limits*.`,
-    `I think about many problems, while some of them haunt me more than others. For example, *what is the analogy of **computational complexity theory** for modern (deep) learning problems?* In other words, how much can we say about the *hardness* of deep learning problems?`,
+    `I think about many problems, while a particular one of them haunts me more than others: *What is the analogy of **computational complexity theory** for modern (deep) learning problems?* In other words, how much can we say about the *hardness* of deep learning problems?`,
     `In my spare time, I enjoy [*photography*](https://street.pbb.wtf/) 📷, writing [*random posts*](./posts) 📝, and staying up late 🌌.`
   ],
   avatar: 'avatar.png',
