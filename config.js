e97876efd35aa950725cf2d7928ea8d21b27ecdd
Pingbang Hu <sleepymalc@gmail.com>
2024-04-24 13:43:48 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   introduction: [
     'I\'m a first-year Ph.D. student at [*University of Illinois Urbana-Champaign*](https://illinois.edu/) 🌽 advised by [Jiaqi Ma](https://jiaqima.github.io/). I obtained a [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from [*University of Michigan*](https://umich.edu/) 〽️ and [*Shanghai Jiao Tong University*](https://www.sjtu.edu.cn/) 🇨🇳. I\'m visiting [National Institute of Informatics](https://www.nii.ac.jp/en/) this summer as a research intern!',
-    `*I do **Trustworthy Machine Learning Theory** with **math** on **graphs***. In particular, I enjoy designing *fast*, *robust*, and *fair* algorithms, and developing their theories. I moved my focus slightly away from **TCS**, specifically, fast graph algorithms recently. But still happy to work on those!`,
+    `*I do **Trustworthy Machine Learning Theory** with **math** on **graphs** (sometimes)*. In particular, I enjoy designing *fast*, *robust*, and *fair* algorithms, and developing their theories. I moved my focus slightly away from **TCS**, specifically, fast graph algorithms recently. But still happy to work on those!`,
     `In my spare time, I do [*street photography*](https://street.pbb.wtf/) 📷 and I'm constantly writing [*course notes*](./posts/Notes) 📝.`,
     `🆕 We publish a [preprint 📝](./research/Unlearning-Sample-Inference-Game) on *Machine Unlearning Evaluation*!`,
   ],
