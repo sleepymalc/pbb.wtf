f2c05902f3548b1fef0020d29e7732b008d25df1
Pingbang Hu <sleepymalc@gmail.com>
2024-05-19 18:17:02 +0900

    📍[main/]

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
   introduction: [
     'I\'m a first-year Ph.D. student at [*University of Illinois Urbana-Champaign*](https://illinois.edu/) 🌽 advised by [Jiaqi Ma](https://jiaqima.github.io/). I obtained my [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from [*University of Michigan*](https://umich.edu/) 〽️ and [*Shanghai Ciao Tong University*](https://www.sjtu.edu.cn/) 🇨🇳.',
     `*I do **Trustworthy Machine Learning Theory** with **math** on **graphs** (sometimes)*. In particular, I enjoy designing *fast*, *robust*, and *fair* algorithms, and developing theories for them.`,
-    `\>I moved my focus slightly away from **Theoretical Computer Science**, specifically, fast graph algorithms. But I still hold a strong interest!`,
+    `\>I moved my focus slightly away from **Theoretical Computer Science** on fast graph algorithms. But I still hold a strong interest!`,
     `In my spare time, I do [*street photography*](https://street.pbb.wtf/) 📷 and I'm constantly writing [*course notes*](./posts/Notes) 📝.`,
     `### 🗞️ News`,
     `* 🎉 We publish a [preprint](https://arxiv.org/abs/2404.11577) 📜 on *Machine Unlearning Evaluation*!`,
