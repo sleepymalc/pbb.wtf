c65c53ac515a2181d212097756dcccad33cc137d
Pingbang Hu <sleepymalc@gmail.com>
2024-05-23 02:29:38 +0900

    Revert "📍[main/]"
    This reverts commit d0d745a6850707ec760ef4924cbbbd7bc3534ce1.

--- config.js
+++ config.js
@@ -28,17 +28,17 @@ module.exports = {
       date: 'Aug. 2023 - Present',
       icon: 'university',
       title: 'Ph.D. in Information Science',
-      location: 'University of Illinois Urbana-Champaign',
+      location: 'School of Information Science, University of Illinois Urbana-Champaign',
     }, {
       date: 'Sep. 2021 - Apr. 2023',
       icon: 'university',
       title: 'B.Sc. in Computer Science (Summa Cum Laude) & minor in Mathematics',
-      location: 'University of Michigan',
+      location: 'College of Engineering & Literature, Science, and the Arts, University of Michigan',
     }, {
       date: 'Sep. 2019 - Aug. 2023',
       icon: 'university',
       title: 'B.Eng. in Electrical and Computer Engineering & minor in Computer Science',
-      location: 'Shanghai Jiao Tong University',
+      location: 'UM-SJTU Joint Institute, Shanghai Jiao Tong University',
     }
   ],
   social: [
