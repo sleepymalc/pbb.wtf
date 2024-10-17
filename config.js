1efcab76d10edaf6c070f595a90d24196a6e50d8
Pingbang Hu <sleepymalc@gmail.com>
2024-10-17 01:07:47 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
 	],
 	introduction: [
 		'I\'m a second-year Ph.D. student at [*University of Illinois Urbana-Champaign*](https://illinois.edu/) 🌽 advised by [Jiaqi Ma](https://jiaqima.github.io/). I obtained my [dual Bachelor degree](https://www.ji.sjtu.edu.cn/academics/ipo/study-abroad/degree-programs/) from [*University of Michigan*](https://umich.edu/) 〽️ and [*Shanghai Jiao Tong University*](https://www.sjtu.edu.cn/) 🇨🇳.',
-		'*I do **machine learning theory** with **math** on **graphs** (sometimes)*, with the goal being to draw theoretical insights from practical problems and to develop algorithms with provable guarantees and desireable properties (e.g., *efficiency*, *robustness*, and *trustworthiness*).',
+		'*I do **machine learning theory** with **math** on **graphs** (sometimes)*, with the goal being to draw theoretical insights from practical problems and develop algorithms with provable guarantees and desireable properties (e.g., *efficiency*, *robustness*, and *fairness*).',
 		'\>Additionally, I hold a strong interest in **Theoretical Computer Science**, especially fast graph algorithms.',
 		'### 🗞️ News',
 		'* 🏆 Received the **NeurIPS 2024 Scholar Award**, see you in Vancouver (*Oct. 11, 2024*)!',
