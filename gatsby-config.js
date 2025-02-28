7c2c19d9a02eb0cbb40a3b8ce0fe86f8d65b4252
Pingbang Hu <sleepymalc@gmail.com>
2025-02-28 02:41:50 -0600

    📍[main/]

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,26 @@
 const config = require('./config');
 
 module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-theme-academic',
-      options: {
-        contentPath: 'content',
-        googleAnalyticTrackingId: config.googleAnalyticTrackingId,
-        defaultLanguage: 'en',
-        pages: config.pages,
-        tagColors: config.tagColors,
-        author: config.author,
-        icon: './static/favicon.png',
-      },
-    },
-  ],
-  siteMetadata: config,
-  pathPrefix: config.pathPrefix,
+	plugins: [
+		{
+			resolve: 'gatsby-theme-academic',
+			options: {
+				contentPath: 'content',
+				googleAnalyticTrackingId: config.googleAnalyticTrackingId,
+				defaultLanguage: 'en',
+				pages: config.pages,
+				tagColors: config.tagColors,
+				author: config.author,
+				icon: './static/favicon.png',
+			},
+		},
+		{
+			resolve: "gatsby-plugin-mdx",
+			options: {
+				remarkPlugins: [require("remark-math")],
+			},
+		},
+	],
+	siteMetadata: config,
+	pathPrefix: config.pathPrefix,
 };
