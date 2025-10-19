f7dd798f599a6c725bcebe7fc89fa92a59ae6375
Pingbang Hu <sleepymalc@gmail.com>
2025-10-19 11:40:01 -0500

    📍[main/]

--- config.js
+++ config.js
@@ -143,126 +143,8 @@ module.exports = {
 	],
 	experience: [
 		{
-			title: 'Teaching',
-			position: 'left',
-			data: [
-				{
-					title: 'Graduate Teaching Assistant',
-					date: 'Jan. 2025 - May 2025',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						'**[IS527 Network Analysis](https://ischool.illinois.edu/degrees-programs/courses/is527)** (Graduate): ' +
-						'An introduction to the theory and applications of social network analysis on the M.S. IS track.',
-				},
-				{
-					title: 'Instructional Aide',
-					date: 'Jan. 2023 - May 2023',
-					location: 'University of Michigan',
-					description:
-						'**[EECS475 Introduction to Cryptography](https://mahdi.ch/eecs475/)** (Undergraduate): ' +
-						'An introduction to the theoretical cryptography on the B.Sc. CS track. [Notes](../posts/Notes#introduction-to-cryptography-eecs475-university-of-michigan-ta) are available.',
-				},
-				{
-					title: 'Instructional Aide',
-					date: 'Aug. 2022 - Dec. 2022',
-					location: 'University of Michigan',
-					description:
-						'**[EECS572 Randomness and Computation](https://mahdi.ch/eecs572/)** (Graduate): ' +
-						'A course about randomness in modern computations on the M.S. CS theory track. [Notes](../posts/Notes#randomness-and-computation-eecs572-university-of-michigan-ta) are available.',
-				},
-				{
-					date: 'May 2021 - Aug. 2021',
-					title: 'Teaching Assistant',
-					location: 'Shanghai Jiao Tong University',
-					description:
-						'**[VV285 Honor Mathematics III](https://umji.sjtu.edu.cn/~horst/teaching/vv285.html)** (Undergraduate): ' +
-						'A follow-up course of VV186 on the B.Eng. ECE track. We also hold the first [*JIntegration Bee* competition](https://github.com/sleepymalc/JIntegrationBee)!',
-				},
-				{
-					title: 'Teaching Assistant',
-					date: 'Sep. 2020 - Dec. 2020',
-					location: 'Shanghai Jiao Tong University',
-					description:
-						'**[VV186 Honor Mathematics II](https://umji.sjtu.edu.cn/~horst/teaching/vv186.html)** (Undergraduate): ' +
-						'The first rigorous analysis course on the B.Eng. ECE track.',
-				},
-			],
-		},
-		{
-			title: 'Professional Service',
-			position: 'left',
-			data: [
-				{
-					title: 'Conference Reviewer',
-					date: 'NA',
-					location: 'Virtual',
-					description:
-						'ICLR *2026*, NeurIPS *2025*, ICLR *2025*, ICML *2024*, IEEE BigData *2023*',
-				},
-				{
-					title: 'Program Committee',
-					date: 'NA',
-					location: 'Virtual',
-					description:
-						'AAAI *2025*',
-				},
-			],
-		},
-		{
-			title: 'Software Development',
-			position: 'left',
-			data: [
-				{
-					title: 'Backend Developer',
-					date: 'Oct. 2020 - June 2021',
-					location: 'Virtual',
-					description:
-						'**foodall**: ' +
-						'A food order platform in Taiwan, built with *Golang*, *Gin*, and *MongoDB*, with RESTful API design.',
-				},
-			],
-		},
-		{
-			title: 'Reading Group',
-			position: 'right',
-			data: [
-				{
-					title: 'Organizer',
-					date: 'June 2024 - Present',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Data Attribution Reading Group**: ` +
-						`Focus on the recent advances of data attribution methods in the era of Generative AI. Please see the [official website](https://trais-lab.github.io/dattri-reading-group/).`,
-				},
-				{
-					title: 'Organizer',
-					date: 'Jan. 2024 - Present',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Statistical Learning Theory**: ` +
-						`A thorough reading on [Roman Vershynin](https://www.math.uci.edu/~rvershyn/)'s [High-Dimensional Probability](https://www.math.uci.edu/~rvershyn/papers/HDP-book/HDP-book.html), focusing on statistical learning theory. [Solution](../else/HDP_solution.pdf) is available.`,
-				},
-				{
-					title: 'Presenter',
-					date: 'Oct. 2023 - Jul. 2024',
-					location: 'Virtual',
-					description:
-						`**[VIDA Reading Group](https://wanghh7.github.io/VIDY-paper-reading-group/)**: ` +
-						`A reading group focuses on recent advances in graph machine learning, and also the foundational theory for statistical learning.`,
-				},
-				{
-					title: 'Organizer',
-					date: 'May 2023 - Dec. 2023',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Sparse Graph Limit Theory**: ` +
-						`A thorough reading on [László Lovász](https://en.wikipedia.org/wiki/L%C3%A1szl%C3%B3_Lov%C3%A1sz)'s [Large networks and graph limits](https://lovasz.web.elte.hu//bookxx/hombook-almost.final.pdf), focusing on Chapter 18 and onwards.`,
-				},
-			],
-		},
-		{
 			title: 'Research and Industry Experience',
-			position: 'right',
+			position: 'left',
 			data: [
 				{
 					title: 'Machine Learning Ph.D. Intern',
@@ -310,6 +192,124 @@ module.exports = {
 					description:
 						'**Deep Learning Foundation**: ' +
 						'Develop various techniques to understand deep neural networks advised by [Wei Hu](https://weihu.me/) under the [SURE Program](https://sure.engin.umich.edu/).',
+				},
+			],
+		},
+		{
+			title: 'Professional Service',
+			position: 'left',
+			data: [
+				{
+					title: 'Conference Reviewer',
+					date: 'NA',
+					location: 'Virtual',
+					description:
+						'ICLR *2026*, NeurIPS *2025*, ICLR *2025*, ICML *2024*, IEEE BigData *2023*',
+				},
+				{
+					title: 'Program Committee',
+					date: 'NA',
+					location: 'Virtual',
+					description:
+						'AAAI *2025*',
+				},
+			],
+		},
+		{
+			title: 'Teaching',
+			position: 'right',
+			data: [
+				{
+					title: 'Graduate Teaching Assistant',
+					date: 'Jan. 2025 - May 2025',
+					location: 'University of Illinois Urbana-Champaign',
+					description:
+						'**[IS527 Network Analysis](https://ischool.illinois.edu/degrees-programs/courses/is527)** (Graduate): ' +
+						'An introduction to the theory and applications of social network analysis on the M.S. IS track.',
+				},
+				{
+					title: 'Instructional Aide',
+					date: 'Jan. 2023 - May 2023',
+					location: 'University of Michigan',
+					description:
+						'**[EECS475 Introduction to Cryptography](https://mahdi.ch/eecs475/)** (Undergraduate): ' +
+						'An introduction to the theoretical cryptography on the B.Sc. CS track. [Notes](../posts/Notes#introduction-to-cryptography-eecs475-university-of-michigan-ta) are available.',
+				},
+				{
+					title: 'Instructional Aide',
+					date: 'Aug. 2022 - Dec. 2022',
+					location: 'University of Michigan',
+					description:
+						'**[EECS572 Randomness and Computation](https://mahdi.ch/eecs572/)** (Graduate): ' +
+						'A course about randomness in modern computations on the M.S. CS theory track. [Notes](../posts/Notes#randomness-and-computation-eecs572-university-of-michigan-ta) are available.',
+				},
+				{
+					date: 'May 2021 - Aug. 2021',
+					title: 'Teaching Assistant',
+					location: 'Shanghai Jiao Tong University',
+					description:
+						'**[VV285 Honor Mathematics III](https://umji.sjtu.edu.cn/~horst/teaching/vv285.html)** (Undergraduate): ' +
+						'A follow-up course of VV186 on the B.Eng. ECE track. We also hold the first [*JIntegration Bee* competition](https://github.com/sleepymalc/JIntegrationBee)!',
+				},
+				{
+					title: 'Teaching Assistant',
+					date: 'Sep. 2020 - Dec. 2020',
+					location: 'Shanghai Jiao Tong University',
+					description:
+						'**[VV186 Honor Mathematics II](https://umji.sjtu.edu.cn/~horst/teaching/vv186.html)** (Undergraduate): ' +
+						'The first rigorous analysis course on the B.Eng. ECE track.',
+				},
+			],
+		},
+		{
+			title: 'Reading Group',
+			position: 'right',
+			data: [
+				{
+					title: 'Organizer',
+					date: 'June 2024 - Present',
+					location: 'University of Illinois Urbana-Champaign',
+					description:
+						`**Data Attribution Reading Group**: ` +
+						`Focus on the recent advances of data attribution methods in the era of Generative AI. Please see the [official website](https://trais-lab.github.io/dattri-reading-group/).`,
+				},
+				{
+					title: 'Organizer',
+					date: 'Jan. 2024 - Present',
+					location: 'University of Illinois Urbana-Champaign',
+					description:
+						`**Statistical Learning Theory**: ` +
+						`A thorough reading on [Roman Vershynin](https://www.math.uci.edu/~rvershyn/)'s [High-Dimensional Probability](https://www.math.uci.edu/~rvershyn/papers/HDP-book/HDP-book.html), focusing on statistical learning theory. [Solution](../else/HDP_solution.pdf) is available.`,
+				},
+				{
+					title: 'Presenter',
+					date: 'Oct. 2023 - Jul. 2024',
+					location: 'Virtual',
+					description:
+						`**[VIDA Reading Group](https://wanghh7.github.io/VIDY-paper-reading-group/)**: ` +
+						`A reading group focuses on recent advances in graph machine learning, and also the foundational theory for statistical learning.`,
+				},
+				{
+					title: 'Organizer',
+					date: 'May 2023 - Dec. 2023',
+					location: 'University of Illinois Urbana-Champaign',
+					description:
+						`**Sparse Graph Limit Theory**: ` +
+						`A thorough reading on [László Lovász](https://en.wikipedia.org/wiki/L%C3%A1szl%C3%B3_Lov%C3%A1sz)'s [Large networks and graph limits](https://lovasz.web.elte.hu//bookxx/hombook-almost.final.pdf), focusing on Chapter 18 and onwards.`,
+				},
+			],
+		},
+		{
+			title: 'Software Development',
+			position: 'right',
+			data: [
+				{
+					title: 'Backend Developer',
+					date: 'Oct. 2020 - June 2021',
+					location: 'Virtual',
+					description:
+						'**foodall**: ' +
+						'A food order platform in Taiwan, built with *Golang*, *Gin*, and *MongoDB*, with RESTful API design.',
 				},
 			],
 		},
